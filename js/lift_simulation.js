6b8909df90a86924ffda52ca78a25d26d6e794ba
Krish-Parekh <krishparekh303@gmail.com>
2023-10-07 10:55:01 +0530

    Github pages navigation issue

--- js/lift_simulation.js
+++ js/lift_simulation.js
@@ -7,7 +7,7 @@ const CURRENT_FLOOR = 1;
 
 
 resetButton.addEventListener("click", () => {
-  window.location.replace("/Lift-Simulation/");
+  window.location.replace("index.html");
 });
 
 class Lift {
